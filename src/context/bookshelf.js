bed7a741b23bd1b2d85fad5921718ca47de25b6d
jcramz <jcramz1992@gmail.com>
2023-06-16 19:43:54 -0500

    refactoring app/adding context

---
+++ src/context/bookshelf.js
@@ -0,0 +1,5 @@
+import { createContent } from "react";
+
+const BookshelfContext = createContent();
+
+export default BookshelfContext;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
+import BookshelfContext from "./context/bookshelf";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <BookshelfContext.Provider value={5}>
+    <App />
+  </BookshelfContext.Provider>
+);
